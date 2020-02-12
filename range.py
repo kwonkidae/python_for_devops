3586da4e1b0f36cda9b708ab609b4ddf3e450cce
kwon <kkdosk@naver.com>
2020-02-12 20:06:20 +0900

    ch01

---
+++ range.py
@@ -0,0 +1,48 @@
+r = range(10)
+print(list(r))
+
+i = 45
+if i == 45:
+    print('i is 45')
+
+i = 35
+if i == 45:
+    print('1 is 45')
+elif i == 35:
+    print('i is 35')
+
+cat = 'spot'
+if 's' in cat:
+    print("Found an 's in a cat")
+
+for i in range(10):
+    x = i * 2
+    print(x)
+
+
+for i in range(6):
+    if i == 3:
+        continue
+    print(i)
+
+count = 0
+while count < 3:
+    print(f"The count is {count}")
+    count += 1
+
+count = 0
+while True:
+    print(f"The count is {count}")
+    if count > 5:
+        break
+    count += 1
+
+thinkers = ['Plato', 'PlayDo', 'Gumby']
+while True:
+    try:
+        thinker = thinkers.pop()
+        print(thinker)
+    except IndexError as e:
+        print("We tried to pop too many thinkers")
+        print(e)
+        break
